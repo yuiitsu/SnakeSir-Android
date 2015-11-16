6c8e578bcd1cb9f3ddbcf23cf323df786d7e0d02
onlyfu <fuwy@foxmail.com>
2015-11-17 00:26:46 +0800

    update

---
+++ app/src/main/java/zoba/me/snakesir/Tools.java
@@ -0,0 +1,17 @@
+package zoba.me.snakesir;
+
+/**
+ * 工具类
+ * Created by onlyfu on 11/16/2015.
+ */
+public class Tools {
+
+    /**
+     * 随机数
+     * @return int
+     */
+    public int random() {
+
+        return 0;
+    }
+}
